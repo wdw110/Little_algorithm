7f7b1ad47d7c967315b3992d28c4a5fb77d02603
wdw110 <wudw110@gmail.com>
2016-08-13 21:13:26 +0800

    add random100.py script

---
+++ 100_random.py
@@ -0,0 +1,19 @@
+#encoding=utf-8
+
+from random import randint
+
+start_list = range(1,10)
+
+def random100(arr):
+	res = list(arr)
+	length = len(arr)
+	for i in range(length):
+		num = randint(1,length)
+		tmp = res[i]
+		res[i] = num
+		res[num-1] = tmp
+	return res
+for i in range(6):
+	print random100(start_list)
+
+
